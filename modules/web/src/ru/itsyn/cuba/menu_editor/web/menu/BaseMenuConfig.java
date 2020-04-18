44769a2abe35c1216e230b58c70247fdbcb68d2f
daring <danila.daring@gmail.com>
2020-04-19 00:43:53 +0300

    #1 - apply default menu

---
+++ modules/web/src/ru/itsyn/cuba/menu_editor/web/menu/BaseMenuConfig.java
@@ -0,0 +1,38 @@
+package ru.itsyn.cuba.menu_editor.web.menu;
+
+import com.haulmont.cuba.core.global.DataManager;
+import com.haulmont.cuba.gui.config.MenuConfig;
+import ru.itsyn.cuba.menu_editor.entity.MenuEntity;
+
+import javax.annotation.Nullable;
+import javax.inject.Inject;
+
+public class BaseMenuConfig extends MenuConfig {
+
+    @Inject
+    protected DataManager dataManager;
+
+    @Override
+    protected void init() {
+        var defMenu = loadDefaultMenu();
+        if (defMenu != null) {
+            rootItems.clear();
+            var re = dom4JTools.readDocument(defMenu.getConfig()).getRootElement();
+            loadMenuItems(re, null);
+            return;
+        }
+        super.init();
+    }
+
+    @Nullable
+    protected MenuEntity loadDefaultMenu() {
+        var query = "select e from menu_MenuEntity e" +
+                " where e.code = :code";
+        return dataManager.load(MenuEntity.class)
+                .query(query)
+                .parameter("code", MenuEntity.DEFAULT_CODE)
+                .optional()
+                .orElse(null);
+    }
+
+}
--- modules/web/src/ru/itsyn/cuba/menu_editor/web/menu/MenuBrowser.java
+++ modules/web/src/ru/itsyn/cuba/menu_editor/web/menu/MenuBrowser.java
@@ -1,11 +1,24 @@
 package ru.itsyn.cuba.menu_editor.web.menu;
 
+import com.haulmont.cuba.gui.components.Action.ActionPerformedEvent;
+import com.haulmont.cuba.gui.config.MenuConfig;
 import com.haulmont.cuba.gui.screen.*;
 import ru.itsyn.cuba.menu_editor.entity.MenuEntity;
+
+import javax.inject.Inject;
 
 @UiController("menu_MenuEntity.browse")
 @UiDescriptor("menu-browse.xml")
 @LookupComponent("table")
 @LoadDataBeforeShow
 public class MenuBrowser extends StandardLookup<MenuEntity> {
+
+    @Inject
+    protected MenuConfig menuConfig;
+
+    @Subscribe("table.apply")
+    public void onMenuApply(ActionPerformedEvent event) {
+        menuConfig.reset();
+    }
+
 }
