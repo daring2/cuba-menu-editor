9a4854344d79f50de50091d1b1759bea1f24354a
daring <danila.daring@gmail.com>
2020-08-27 12:20:46 +0300

    #1 - MenuBrowser - add copy action

--- modules/web/src/ru/itsyn/cuba/menu_editor/web/menu/MenuBrowser.java
+++ modules/web/src/ru/itsyn/cuba/menu_editor/web/menu/MenuBrowser.java
@@ -1,6 +1,10 @@
 package ru.itsyn.cuba.menu_editor.web.menu;
 
+import com.haulmont.cuba.core.global.Metadata;
+import com.haulmont.cuba.core.global.UuidProvider;
+import com.haulmont.cuba.gui.ScreenBuilders;
 import com.haulmont.cuba.gui.components.Action.ActionPerformedEvent;
+import com.haulmont.cuba.gui.components.Table;
 import com.haulmont.cuba.gui.screen.*;
 import ru.itsyn.cuba.menu_editor.entity.MenuEntity;
 
@@ -13,11 +17,30 @@ import javax.inject.Inject;
 public class MenuBrowser extends StandardLookup<MenuEntity> {
 
     @Inject
+    protected Metadata metadata;
+    @Inject
+    protected ScreenBuilders screenBuilders;
+    @Inject
     protected AppMenuManager appMenuManager;
+    @Inject
+    protected Table<MenuEntity> table;
 
     @Subscribe("table.apply")
     public void onMenuApply(ActionPerformedEvent event) {
         appMenuManager.reloadAppMenu();
+    }
+
+    @Subscribe("table.copy")
+    public void onMenuCopy(ActionPerformedEvent event) {
+        var entity = table.getSingleSelected();
+        if (entity == null)
+            return;
+        var ce = metadata.getTools().copy(entity);
+        ce.setId(UuidProvider.createUuid());
+        ce.setName(ce.getName() + " - Copy");
+        screenBuilders.editor(table)
+                .newEntity(ce)
+                .show();
     }
 
 }
