21c65c40d0f316358123e7db3a4f7b33535c6614
daring <danila.daring@gmail.com>
2020-09-07 13:09:06 +0300

    #1 - MenuItemEditor - add updateFields method

--- modules/web/src/ru/itsyn/cuba/menu_editor/web/menu_item/MenuItemEditor.java
+++ modules/web/src/ru/itsyn/cuba/menu_editor/web/menu_item/MenuItemEditor.java
@@ -1,8 +1,11 @@
 package ru.itsyn.cuba.menu_editor.web.menu_item;
 
+import com.haulmont.cuba.gui.components.CheckBox;
 import com.haulmont.cuba.gui.components.LookupField;
 import com.haulmont.cuba.gui.screen.*;
 import ru.itsyn.cuba.menu_editor.entity.MenuItemEntity;
+import ru.itsyn.cuba.menu_editor.entity.MenuItemType;
+import ru.itsyn.cuba.menu_editor.entity.MenuOpenType;
 
 import javax.inject.Inject;
 
@@ -15,11 +18,26 @@ import static java.util.Arrays.asList;
 public class MenuItemEditor extends StandardEditor<MenuItemEntity> {
 
     @Inject
+    protected LookupField<MenuItemType> itemTypeField;
+    @Inject
+    protected LookupField<MenuOpenType> openTypeField;
+    @Inject
     protected LookupField<Boolean> resizableField;
+    @Inject
+    protected CheckBox expandedField;
 
     @Subscribe
     public void onInit(InitEvent event) {
         resizableField.setOptionsList(asList(Boolean.TRUE, Boolean.FALSE));
+        itemTypeField.addValueChangeListener(e -> updateFields());
+        openTypeField.addValueChangeListener(e -> updateFields());
+    }
+
+    protected void updateFields() {
+        var itemType= itemTypeField.getValue();
+        var openType = openTypeField.getValue();
+        resizableField.setEditable(itemType == MenuItemType.SCREEN && openType == MenuOpenType.DIALOG);
+        expandedField.setEditable(itemType == MenuItemType.MENU);
     }
 
 }
