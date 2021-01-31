6a0f0935a19f7b533e5f7a2ae3c66d6714774802
daring <danila.daring@gmail.com>
2021-01-31 18:31:16 +0300

    #1 - add tests

--- modules/web/test/ru/itsyn/cuba/menu_editor/web/menu_item/MenuConfigBuilderTest.java
+++ modules/web/test/ru/itsyn/cuba/menu_editor/web/menu_item/MenuConfigBuilderTest.java
@@ -1,9 +1,11 @@
 package ru.itsyn.cuba.menu_editor.web.menu_item;
 
+import org.dom4j.DocumentHelper;
 import org.junit.jupiter.api.Test;
 import ru.itsyn.cuba.menu_editor.entity.MenuItemType;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 class MenuConfigBuilderTest {
 
@@ -14,6 +16,21 @@ class MenuConfigBuilderTest {
         assertEquals("separator", builder.getElementName(MenuItemType.SEPARATOR));
         assertEquals("item", builder.getElementName(MenuItemType.SCREEN));
         assertEquals("item", builder.getElementName(null));
+    }
+
+    @Test
+    void testAddAttributeValue() {
+        var builder = new MenuConfigBuilder();
+        var e = DocumentHelper.createElement("e1");
+        builder.addAttributeValue(e, "a1", null);
+        assertNull(e.attribute("a1"));
+        assertEquals(0, e.attributeCount());
+        builder.addAttributeValue(e, "a1", "v1");
+        assertEquals("v1", e.attribute("a1").getValue());
+        builder.addAttributeValue(e, "a2", 2);
+        assertEquals("2", e.attribute("a2").getValue());
+        builder.addAttributeValue(e, "a3", MenuItemType.MENU);
+        assertEquals("MENU", e.attribute("a3").getValue());
     }
 
 }
